7fbb5f8145bde84d7ee02cdcd433b530db6226c0
dengjingkun <dengjingkun@shgbit.com>
2023-10-13 13:50:38 +0800

    Initial Commit

---
+++ CoreBluetoothControl/CoreBluetoothControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothControlApp.swift
+//  CoreBluetoothControl
+//
+//  Created by 邓璟琨 on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
